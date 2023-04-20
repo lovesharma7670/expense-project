f8d460aafab10ec7fac4e7f0bc2de4b9b5947e1e
lovesharma7670 <119242996+lovesharma7670@users.noreply.github.com>
2023-04-20 23:21:25 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require("cors")
 const app = express();
 const mongoose = require("mongoose");
-const { db } = require('./db/db');
+const connectDB = require("./db/connect");
 const {readdirSync} = require("fs")
 
 
@@ -17,7 +17,7 @@ if (port == null || port == "") {
     port = 3000;
 }
 
-mongoose.connect("mongodb://0.0.0.0:27017/Income");
+
 // mongoose.connect('mongodb+srv://lovesharma7670:love7670@cluster0.ssdvusi.mongodb.net/Income');
 
  
@@ -31,7 +31,7 @@ app.get("/", function(req,res){
 readdirSync('./router').map((route) => app.use('/api/v1', require ("./router/" + route)))
 
 const server = () => {
-    
+    connectDB();
     app.listen(port, function () {
         console.log("The server has started successfully", port);
     })
